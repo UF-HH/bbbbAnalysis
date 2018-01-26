eb6bee0e14171fc372cb3d5180a7bc5a1b7eb70e
Luca Cadamuro <lc.cadamuro@gmail.com>
2018-01-26 08:45:43 -0600

    composite candidates class

---
+++ interface/CompositeCandidate.h
@@ -0,0 +1,57 @@
+#ifndef COMPOSITECANDIDATE_H
+#define COMPOSITECANDIDATE_H
+
+/*
+** class  : CompositeCandidate
+** author : L. Cadamuro (UF)
+** date   : 23/01/2018
+** brief  : an object composed by a pair of Candidates
+** note   : the candidate stores the combined P4 and keep reference of its components
+**        : this reference is in the form of the two idx of the components, plus the two components themselves
+**        : The CompositeCandidate own completely the two components and any copy operation also creates two new components
+**        : This should not cause a problem as the == comparison between to Candidates is based on their index
+**        : but be careful in case you want to compare the pointers
+*/
+
+#include "TLorentzVector.h"
+#include "Candidate.h"
+#include <utility>
+
+class CompositeCandidate
+{
+    public:
+        CompositeCandidate() : cand1_(), cand2_() {p4_.SetPxPyPzE(0,0,0,0); idx1_ = -1; idx2_ = -1;}
+        CompositeCandidate(const Candidate& c1, const Candidate& c2) {setComponents(c1,c2);}
+        ~CompositeCandidate(){};
+        CompositeCandidate(const CompositeCandidate& rhs); // copy ctor
+        CompositeCandidate& operator = (const CompositeCandidate& rhs);        // assignment
+        
+        TLorentzVector P4() const      {return p4_;}
+        
+        void setComponents(const Candidate& c1, const Candidate& c2);
+
+        int                getIdxComponent1 () const {return idx1_;}
+        int                getIdxComponent2 () const {return idx2_;}
+        std::pair<int,int> getIdxComponents () const {return std::make_pair(getIdxComponent1(), getIdxComponent2());}
+
+        Candidate&                       getComponent1 () const {return (*cand1_);}
+        Candidate&                       getComponent2 () const {return (*cand2_);}
+        
+        bool sharesComponentWith (const CompositeCandidate& cc) const;
+        bool isValid() {return (cand1_ && cand2_);}
+        // void setP4( TLorentzVector p4) {p4_ = p4;}
+        // int getIdx() const {return idx_;}
+        // NanoAODTree* getNanoAODTree() const {return nat_;}
+
+    protected:
+        // virtual void buildP4(NanoAODTree* nat) = 0;
+        // int idx_;
+        TLorentzVector p4_;
+        int idx1_;
+        int idx2_;
+        std::unique_ptr<Candidate> cand1_;
+        std::unique_ptr<Candidate> cand2_;
+        // NanoAODTree* nat_;
+};
+
+#endif
---
+++ src/CompositeCandidate.cc
@@ -0,0 +1,39 @@
+#include "CompositeCandidate.h"
+
+void CompositeCandidate::setComponents(const Candidate& c1, const Candidate& c2)
+{
+    p4_ = (c1.P4() + c2.P4());
+    idx1_ = c1.getIdx();
+    idx2_ = c2.getIdx();
+    cand1_ = c1.clone();
+    cand2_ = c2.clone();
+    return;
+}
+
+bool CompositeCandidate::sharesComponentWith(const CompositeCandidate& cc) const
+{
+    if (this->getIdxComponent1() == cc.getIdxComponent1()) return true;
+    if (this->getIdxComponent1() == cc.getIdxComponent2()) return true;
+    if (this->getIdxComponent2() == cc.getIdxComponent1()) return true;
+    if (this->getIdxComponent2() == cc.getIdxComponent2()) return true;
+    return false;
+}
+
+CompositeCandidate::CompositeCandidate(const CompositeCandidate& rhs)
+{
+    this -> p4_ = rhs.p4_;
+    this -> idx1_ = rhs.idx1_;
+    this -> idx2_ = rhs.idx2_;
+    this -> cand1_ = rhs.cand1_->clone();
+    this -> cand2_ = rhs.cand2_->clone();
+}
+
+CompositeCandidate& CompositeCandidate::operator = (const CompositeCandidate& rhs)
+{
+    this -> p4_ = rhs.p4_;
+    this -> idx1_ = rhs.idx1_;
+    this -> idx2_ = rhs.idx2_;
+    this -> cand1_ = rhs.cand1_->clone();
+    this -> cand2_ = rhs.cand2_->clone();
+    return (*this);
+}

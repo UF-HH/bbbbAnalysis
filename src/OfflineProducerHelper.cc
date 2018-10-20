84290f57c11c64729710a202e5dc0961983ebe4e
fravera <fabio.ravera89@gmail.com>
2018-10-19 23:32:21 -0500

    solved bug in the min number of CSV bjets

--- src/OfflineProducerHelper.cc
+++ src/OfflineProducerHelper.cc
@@ -271,10 +271,14 @@ std::vector<Jet> OfflineProducerHelper::bbbb_jets_idxs_HighestCSVandClosestToMh(
 
     //Jets are already ordered form highest to lowest CSV
     for(; jetsPassingDeepCSV < jets->size(); ++jetsPassingDeepCSV){
+        cout<<jetsPassingDeepCSV<<" ";
         float tmpDeepCSV = get_property(jets->at(jetsPassingDeepCSV),Jet_btagDeepB);
         // cout<<"Before regression - id "<< jetsPassingDeepCSV << " pt " << jets->at(jetsPassingDeepCSV).P4().Pt() << endl;
         if(tmpDeepCSV < minimumDeepCSVaccepted) break;
     }
+    ++jetsPassingDeepCSV;
+
+    cout<<"Result = "<<jetsPassingDeepCSV<<endl;
 
     std::vector<Jet> output_jets;
     if(jetsPassingDeepCSV < 4) return output_jets;

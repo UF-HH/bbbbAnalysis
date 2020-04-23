44710e770578c2133921cbe7fb13f45f54b93b19
daniel <daniel.guerrero@cern.ch>
2020-04-23 09:47:01 -0500

    VBF line scripts

--- limits/vbflines/VBF_cv.py
+++ limits/vbflines/VBF_cv.py
@@ -87,7 +87,7 @@ def calculatecoeffients(sample_list,couplingstoscan):
     coeffs = c.transpose() * Minv # coeffs * s is the sigma, accessing per component gives each sample scaling
     sigma  = coeffs*s  
 
-    with open('config/vbfhh4bforcvscan.txt', 'w') as the_file:
+    with open('config/vbfhhcvline.txt', 'w') as the_file:
      for j in range(0, len(couplingstoscan)):
          ###### target values
          couplings = couplingstoscan[j]
@@ -109,49 +109,23 @@ def calculatecoeffients(sample_list,couplingstoscan):
          for k in range(0, len(factors) ):
             if abs(factors[k]) == 0: factors[k]=0 
             if abs(factors[k]) < 0.001: factors[k]=0 
-         print ">>> XS = %f, target (CV,C2V,kl) = (%.1f,%.1f, %.1f), factors: (f1,f2,f3,f4,f5,f6) = (%.1f,%.1f,%.1f,%.1f,%.1f,%.1f)" %(xsec, t_CV, t_C2V, t_kl, factors[0],factors[1],factors[2],factors[3],factors[4],factors[5] )  
-         the_file.write('%f %.1f %.1f %.1f %.3f %.3f %.3f %.3f %.3f %.3f \n'%(xsec, t_CV, t_C2V, t_kl, factors[0],factors[1],factors[2],factors[3],factors[4],factors[5] ) )
-
-    with open('config/vbfhh4bcvline.txt', 'w') as the_file:
-     for j in range(0, len(couplingstoscan)):
-         ###### target values
-         couplings = couplingstoscan[j]
-         t_CV  = couplings[0]
-         t_C2V = couplings[1]
-         t_kl  = couplings[2]  
-         xsec  = sigma[0].evalf(subs={
-             CV  : t_CV,
-             C2V : t_C2V,
-             kl  : t_kl,
-             s1  : sample_list[0].val_xs,
-             s2  : sample_list[1].val_xs,
-             s3  : sample_list[2].val_xs,
-             s4  : sample_list[3].val_xs,
-             s5  : sample_list[4].val_xs,
-             s6  : sample_list[5].val_xs,
-         }) 
-         factors = [coeffs[x].evalf(subs={CV : t_CV, C2V : t_C2V, kl : t_kl}) for x in range(0,6)]
-         for k in range(0, len(factors) ):
-            if abs(factors[k]) == 0: factors[k]=0 
-            if abs(factors[k]) < 0.001: factors[k]=0 
-         print ">>> XS = %f, target (CV,C2V,kl) = (%.2f,%.1f, %.1f)" %(xsec, t_CV, t_C2V, t_kl )  
-         the_file.write('%.2f %f\n'%(t_CV,xsec/(1)))
+         print ">>> XS = %f, target (CV,C2V,kl) = (%.2f,%.1f,%.1f)" %((xsec)/(1), t_CV, t_C2V, t_kl )  
+         the_file.write('%.2f %.5f\n'%(t_CV,(xsec)/(1)))
 
 #################################################################################################
 print "WELCOME TO THE VBF-HH CALCULATOR"
 #################################################################################################
 #Create an array with samples couplings(cv,c2v,kl) and their L0 xs
 sample_list = [
-    sample(1,1,1,   val_xs = 0.00054),
-    sample(1,2,1,   val_xs = 0.00472),
-    sample(1,1,2,   val_xs = 0.00044),
-    sample(1,1,0,   val_xs = 0.00145),
-    sample(0.5,1,1, val_xs = 0.00353),
-    sample(1.5,1,1, val_xs = 0.02149)
+    sample(1,1,1,   val_xs = 0.000585),
+    sample(1,2,1,   val_xs = 0.004688),
+    sample(1,1,2,   val_xs = 0.000482),
+    sample(1,1,0,   val_xs = 0.001585),
+    sample(0.5,1,1, val_xs = 0.003586),
+    sample(1.5,1,1, val_xs = 0.022297)
 ]  
 #Create an array with couplings (cv,c2v,kl) to scan. In our case is c2v scan.
 couplingstoscan_list = [ [-3.0 + (x*0.05),1,1] for x in range(0,121)]     
 #Compute coefficients to be used during histogram building and save them in a text file
 print "[INFO] VBF-HH CALCULATOR RESULTS"
-print couplingstoscan_list
 calculatecoeffients(sample_list,couplingstoscan_list)

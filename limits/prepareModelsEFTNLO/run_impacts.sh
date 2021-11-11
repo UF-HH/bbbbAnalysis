46ae48be54a589ba7c1380817ed5a500e7a66a8e
Daniel Fernando Guerrero Ibarra <daniel.guerrero@cern.ch>
2021-11-11 11:03:16 -0600

    Datacard scripts for EFT nodes

---
+++ limits/prepareModelsEFTNLO/run_impacts.sh
@@ -0,0 +1,12 @@
+#GGF impacts
+
+source make_impacts.sh 2016 1 12.5
+source make_impacts.sh 20172018 1 8.9
+
+
+#VBF impacts
+
+source make_impacts.sh 2016 2 753.1
+source make_impacts.sh 20172018 2 637.5
+
+
